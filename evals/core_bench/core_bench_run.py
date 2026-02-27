f5f2b050b18fe8461656547e0a8524173dc1d403
jmohl <jefftmohl@gmail.com>
2026-02-27 12:48:34 -0700

    adjusting to make more performant

--- evals/core_bench/core_bench_run.py
+++ evals/core_bench/core_bench_run.py
@@ -25,23 +25,30 @@ from inspect_evals.core_bench import core_bench
 
 LOG_DIR = str(Path(__file__).parent / "eval-logs")
 
-# Create an agent that uses bash and python
+# Create an agent that uses bash, python, and a text editor
 agent = basic_agent(tools=[bash(timeout=180), python(), text_editor()])
 task = core_bench(
     #language="Python",
-    limit=50, #limit = 0 to run all
+    limit=0, #limit = 0 to run all
     solver=agent,
-    #difficulty='easy',
-    filter_out_gpu=True,
+    difficulty='easy', #tasks are either easy, medium, or hard. medium and hard tasks may use gpu resources
+    #filter_out_gpu=False, #note that tasks will fail if there is no GPU on your machine
+    token_limit=1000000, #original CORE-bench had a $4 limit per task, can use this to approximate that
+    max_messages=50
 )
-# See how agent performs
-eval(task, model="openai/gpt-5-mini-2025-08-07", log_dir=LOG_DIR)
 
+# See how agent performs
+# Note I set these sandboxes and subprocesses to lower levels because I was getting instability, but this makes it run more slowly
+eval(
+    task, 
+    model="openai/gpt-5-mini-2025-08-07",
+    max_sandboxes=8,
+    max_subprocesses=8,
+    max_connections=20, 
+    log_dir=LOG_DIR) #gpt-5.2-2025-12-11
+
+
+#run with python evals/core_bench/core_bench_run.py
 #then use [inspect view --log-dir "evals/core_bench/eval-logs"] to see results
 
 
-
-
-## Running from command line
-# this is another way to do this instead of running this script. Need to pass in your options
-# inspect eval inspect_evals/core_bench --model openai/gpt-5-nano --limit 1 
